b872c2e30dcf82ad260015cf2d573f2ae9d23b91
+ <+>
2023-03-24 08:47:18 +0300

    completed DOM modyfying elements lab

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
 // Write your code here!
+
+// remove the main element with id 'main'
+document.querySelector('main#main').remove();
+
+// create a new h1 element with id 'victory' and text content
+const newHeader = document.createElement('h1');
+newHeader.id = 'victory';
+newHeader.textContent = 'Kelly is the champion';
+
+// append the new h1 element to the body
+document.body.appendChild(newHeader);
